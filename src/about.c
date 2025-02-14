470c485686a6929abd4e5a565cfba3b0a621e1f0
Boris Yumankulov <boria138@altlinux.org>
2025-02-14 12:48:03 +0500

    Rewrite about.c to newest GTK3

--- src/about.c
+++ src/about.c
@@ -14,94 +14,94 @@
  * You should have received a copy of the GNU General Public License
  * along with YAD. If not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright (C) 2008-2024, Victor Ananjevsky <victor@sanana.kiev.ua>
+ * Copyright (C) 2008-2025, Victor Ananjevsky <victor@sanana.kiev.ua>
  */
 
 #include "yad.h"
 
 static void
-yad_set_about_license (GtkWidget *dlg)
+yad_set_about_license (GtkAboutDialog *dlg)
 {
   if (options.about_data.license == NULL)
     {
-      gtk_about_dialog_set_license_type (GTK_ABOUT_DIALOG (dlg), GTK_LICENSE_UNKNOWN);
+      gtk_about_dialog_set_license_type (dlg, GTK_LICENSE_UNKNOWN);
       return;
     }
 
-  gtk_about_dialog_set_wrap_license (GTK_ABOUT_DIALOG (dlg), TRUE);
+  gtk_about_dialog_set_wrap_license (dlg, TRUE);
 
-  /* check for predefined */
-  if (strncmp (options.about_data.license, "GPL2", 4) == 0)
+  /* Check for predefined licenses */
+  if (g_strcmp0 (options.about_data.license, "GPL-2.0") == 0)
     {
-      gtk_about_dialog_set_license_type (GTK_ABOUT_DIALOG (dlg), GTK_LICENSE_GPL_2_0);
+      gtk_about_dialog_set_license_type (dlg, GTK_LICENSE_GPL_2_0);
       return;
     }
-  if (strncmp (options.about_data.license, "GPL3", 4) == 0)
+  if (g_strcmp0 (options.about_data.license, "GPL-3.0") == 0)
     {
-      gtk_about_dialog_set_license_type (GTK_ABOUT_DIALOG (dlg), GTK_LICENSE_GPL_3_0);
+      gtk_about_dialog_set_license_type (dlg, GTK_LICENSE_GPL_3_0);
       return;
     }
-  if (strncmp (options.about_data.license, "LGPL2", 5) == 0)
+  if (g_strcmp0 (options.about_data.license, "LGPL-2.1") == 0)
     {
-      gtk_about_dialog_set_license_type (GTK_ABOUT_DIALOG (dlg), GTK_LICENSE_LGPL_2_1);
+      gtk_about_dialog_set_license_type (dlg, GTK_LICENSE_LGPL_2_1);
       return;
     }
-  if (strncmp (options.about_data.license, "LGPL3", 5) == 0)
+  if (g_strcmp0 (options.about_data.license, "LGPL-3.0") == 0)
     {
-      gtk_about_dialog_set_license_type (GTK_ABOUT_DIALOG (dlg), GTK_LICENSE_LGPL_3_0);
+      gtk_about_dialog_set_license_type (dlg, GTK_LICENSE_LGPL_3_0);
       return;
     }
-  if (strncmp (options.about_data.license, "BSD", 3) == 0)
+  if (g_strcmp0 (options.about_data.license, "BSD") == 0)
     {
-      gtk_about_dialog_set_license_type (GTK_ABOUT_DIALOG (dlg), GTK_LICENSE_BSD);
+      gtk_about_dialog_set_license_type (dlg, GTK_LICENSE_BSD);
       return;
     }
-  if (strncmp (options.about_data.license, "MIT", 3) == 0)
+  if (g_strcmp0 (options.about_data.license, "MIT") == 0)
     {
-      gtk_about_dialog_set_license_type (GTK_ABOUT_DIALOG (dlg), GTK_LICENSE_MIT_X11);
+      gtk_about_dialog_set_license_type (dlg, GTK_LICENSE_MIT_X11);
       return;
     }
-  if (strncmp (options.about_data.license, "ARTISTIC", 8) == 0)
+  if (g_strcmp0 (options.about_data.license, "Artistic") == 0)
     {
-      gtk_about_dialog_set_license_type (GTK_ABOUT_DIALOG (dlg), GTK_LICENSE_ARTISTIC);
+      gtk_about_dialog_set_license_type (dlg, GTK_LICENSE_ARTISTIC);
       return;
     }
 
-  /* user specified */
-  gtk_about_dialog_set_license_type (GTK_ABOUT_DIALOG (dlg), GTK_LICENSE_CUSTOM);
+  /* User specified license */
+  gtk_about_dialog_set_license_type (dlg, GTK_LICENSE_CUSTOM);
   if (g_file_test (options.about_data.license, G_FILE_TEST_EXISTS))
     {
       gchar *buf;
 
       if (g_file_get_contents (options.about_data.license, &buf, NULL, NULL))
         {
-          gtk_about_dialog_set_license (GTK_ABOUT_DIALOG (dlg), buf);
+          gtk_about_dialog_set_license (dlg, buf);
+          g_free (buf);
           return;
         }
     }
 
-  /* set as is */
-  gtk_about_dialog_set_license (GTK_ABOUT_DIALOG (dlg), options.about_data.license);
-
-  return;
+  /* Set license as is */
+  gtk_about_dialog_set_license (dlg, options.about_data.license);
 }
 
 gint
 yad_about (void)
 {
   GtkWidget *dialog;
+  GtkAboutDialog *about;
 
   const gchar *const authors[] = {
     "Victor Ananjevsky <victor@sanana.kiev.ua>",
     NULL
   };
-  const gchar *translators = N_("translator-credits");
+  const gchar *translators = _("translator-credits");
 
   gchar *comments = g_strdup_printf (_("Yet Another Dialog\n"
                                        "(show dialog boxes from shell scripts)\n"
                                        "\nBased on Zenity code\n\n"
 #ifdef HAVE_HTML
-                                       "Built with Webkit\n"
+                                       "Built with WebKit\n"
 #endif
 #ifdef HAVE_SOURCEVIEW
                                        "Built with GtkSourceView\n"
@@ -109,10 +109,13 @@ yad_about (void)
 #ifdef HAVE_SPELL
                                        "Built with GSpell\n"
 #endif
-                                       "Using GTK+ %d.%d.%d\n"),
-                                     gtk_major_version, gtk_minor_version, gtk_micro_version);
+                                       "Using GTK+ %s\n"),
+                                     gtk_get_version ());
 
   dialog = gtk_about_dialog_new ();
+  about = GTK_ABOUT_DIALOG (dialog);
+
+  /* Set window icon */
   if (options.data.window_icon)
     gtk_window_set_icon_name (GTK_WINDOW (dialog), options.data.window_icon);
   else
@@ -120,39 +123,63 @@ yad_about (void)
 
   if (options.about_data.name != NULL)
     {
-      /* custom about dialog */
-      gtk_about_dialog_set_program_name (GTK_ABOUT_DIALOG (dialog), options.about_data.name);
+      /* Custom about dialog */
+      gtk_about_dialog_set_program_name (about, options.about_data.name);
+
       if (options.data.dialog_image)
-        gtk_about_dialog_set_logo (GTK_ABOUT_DIALOG (dialog), get_pixbuf (options.data.dialog_image, YAD_BIG_ICON, TRUE));
+        {
+          GdkPixbuf *pixbuf = get_pixbuf (options.data.dialog_image, YAD_BIG_ICON, TRUE);
+          gtk_about_dialog_set_logo (about, pixbuf);
+          if (pixbuf)
+            g_object_unref (pixbuf);
+        }
+
       if (options.about_data.version)
-        gtk_about_dialog_set_version (GTK_ABOUT_DIALOG (dialog), options.about_data.version);
+        gtk_about_dialog_set_version (about, options.about_data.version);
+
       if (options.about_data.copyright)
-        gtk_about_dialog_set_copyright (GTK_ABOUT_DIALOG (dialog), options.about_data.copyright);
+        gtk_about_dialog_set_copyright (about, options.about_data.copyright);
+
       if (options.about_data.comments)
-        gtk_about_dialog_set_comments (GTK_ABOUT_DIALOG (dialog), options.about_data.comments);
+        gtk_about_dialog_set_comments (about, options.about_data.comments);
+
       if (options.about_data.authors)
-        gtk_about_dialog_set_authors (GTK_ABOUT_DIALOG (dialog), (const gchar **) g_strsplit (options.about_data.authors, ",", -1));
+        {
+          gchar **authors_list = g_strsplit (options.about_data.authors, ",", -1);
+          gtk_about_dialog_set_authors (about, (const gchar **) authors_list);
+          g_strfreev (authors_list);
+        }
+
       if (options.about_data.website)
-        gtk_about_dialog_set_website (GTK_ABOUT_DIALOG (dialog), options.about_data.website);
+        gtk_about_dialog_set_website (about, options.about_data.website);
+
       if (options.about_data.website_lbl)
-        gtk_about_dialog_set_website_label (GTK_ABOUT_DIALOG (dialog), options.about_data.website_lbl);
-      yad_set_about_license (dialog);
+        gtk_about_dialog_set_website_label (about, options.about_data.website_lbl);
+
+      yad_set_about_license (about);
     }
   else
     {
-      g_object_set (G_OBJECT (dialog),
-                    "name", PACKAGE_NAME,
-                    "version", PACKAGE_VERSION,
-                    "copyright", "Copyright \xc2\xa9 2008-2024, Victor Ananjevsky <victor@sanana.kiev.ua>",
-                    "comments", comments,
-                    "authors", authors,
-                    "website", PACKAGE_URL,
-                    "translator-credits", translators,
-                    "wrap-license", TRUE,
-                    "license-type", GTK_LICENSE_GPL_3_0,
-                    "logo-icon-name", "yad",
-                    NULL);
+      /* Default YAD about dialog */
+      gtk_about_dialog_set_program_name (about, PACKAGE_NAME);
+      gtk_about_dialog_set_version (about, PACKAGE_VERSION);
+      gtk_about_dialog_set_copyright (about, "Copyright © 2008-2025, Victor Ananjevsky <victor@sanana.kiev.ua>");
+      gtk_about_dialog_set_comments (about, comments);
+      gtk_about_dialog_set_authors (about, authors);
+      gtk_about_dialog_set_website (about, PACKAGE_URL);
+      gtk_about_dialog_set_translator_credits (about, translators);
+      gtk_about_dialog_set_wrap_license (about, TRUE);
+      gtk_about_dialog_set_license_type (about, GTK_LICENSE_GPL_3_0);
+      gtk_about_dialog_set_logo_icon_name (about, "yad");
     }
 
-  return gtk_dialog_run (GTK_DIALOG (dialog));
+  g_free (comments);
+
+  /* Connect response signal to close window */
+  g_signal_connect_swapped (dialog, "response",
+                            G_CALLBACK (gtk_widget_destroy), dialog);
+
+  gtk_widget_show (dialog);
+
+  return 0;
 }
